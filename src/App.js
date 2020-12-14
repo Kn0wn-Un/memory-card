33a0fb832f955413efe993be7cbb3385232475a3
Kn0wn-Un <naikdarsh175@gmail.com>
2020-12-14 13:58:06 +0530

    handler lifted to App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ function App() {
     const arr = [...Array(12).keys()];
     const [score, setScore] = useState(0);
     const [highS, setHighS] = useState(0);
+    const [clicked, setClicked] = useState([]);
+    const checkClicked = (ele) => {
+        if (clicked.indexOf(ele) === -1) {
+            setScore(score + 1);
+            clicked.push(ele);
+            setClicked(clicked);
+        } else {
+            setScore(0);
+            setClicked([]);
+        }
+    };
     useEffect(() => {
         if (score > highS) setHighS(score);
     }, [score, highS]);
     return (
         <div className="App">
             <NavBar score={score} highScore={highS} />
-            <ShowCards arr={arr} handler={setScore} score={score} />
-            <button
-                onClick={() => {
-                    setScore(0);
-                }}
-            >
-                Reset
-            </button>
+            <ShowCards arr={arr} handler={checkClicked} score={score} />
         </div>
     );
 }
